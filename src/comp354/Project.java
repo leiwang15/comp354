6bd9b327509298e0cc7e26c72b703a5b4988c624
Lei <leiwang15@gmail.com>
2015-05-28 18:50:17 -0400

    Create basic db tables

--- src/comp354/Project.java
+++ src/comp354/Project.java
@@ -1,5 +1,85 @@
 package comp354;
 
-public class Project {
+import java.util.Date;
+import java.util.List;
 
+public class Project {
+	private int project_id;
+	private String project_name;
+	private String project_desc;
+	private Date start_date;
+	private Date end_date;
+	private List<Activity> activities;
+	/**
+	 * @return the project_id
+	 */
+	public int getProject_id() {
+		return project_id;
+	}
+	/**
+	 * @param project_id the project_id to set
+	 */
+	public void setProject_id(int project_id) {
+		this.project_id = project_id;
+	}
+	/**
+	 * @return the project_name
+	 */
+	public String getProject_name() {
+		return project_name;
+	}
+	/**
+	 * @param project_name the project_name to set
+	 */
+	public void setProject_name(String project_name) {
+		this.project_name = project_name;
+	}
+	/**
+	 * @return the project_desc
+	 */
+	public String getProject_desc() {
+		return project_desc;
+	}
+	/**
+	 * @param project_desc the project_desc to set
+	 */
+	public void setProject_desc(String project_desc) {
+		this.project_desc = project_desc;
+	}
+	/**
+	 * @return the start_date
+	 */
+	public Date getStart_date() {
+		return start_date;
+	}
+	/**
+	 * @param start_date the start_date to set
+	 */
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	/**
+	 * @return the end_date
+	 */
+	public Date getEnd_date() {
+		return end_date;
+	}
+	/**
+	 * @param end_date the end_date to set
+	 */
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	/**
+	 * @return the activities
+	 */
+	public List<Activity> getActivities() {
+		return activities;
+	}
+	/**
+	 * @param activities the activities to set
+	 */
+	public void setActivities(List<Activity> activities) {
+		this.activities = activities;
+	}
 }
--- src/comp354/User.java
+++ src/comp354/User.java
@@ -1,5 +1,44 @@
 package comp354;
-
+import java.lang.String;
 public class User {
+	private String first_name;
+	private String last_name;
+	private String role;
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	/**
+	 * @return the last_name
+	 */
+	public String getLast_name() {
+		return last_name;
+	}
+	/**
+	 * @param last_name the last_name to set
+	 */
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	/**
+	 * @return the first_name
+	 */
+	public String getFirst_name() {
+		return first_name;
+	}
+	/**
+	 * @param first_name the first_name to set
+	 */
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
 
 }
