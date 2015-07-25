24f43e2d075c2e59bff5b2269546cf5f4c36bdf0
Joao Miranda <jmiranda@masterfile.com>
2015-07-25 16:27:17 -0400

    Added PERT table columns.

--- src/edu/concordia/comp354/gui/ActivityEntry.java
+++ src/edu/concordia/comp354/gui/ActivityEntry.java
@@ -38,14 +38,14 @@ import java.util.List;
 /**
  * Created by joao on 15.06.05.
  */
-public class ActivityEntry extends BaseTab implements IActivityEntryRenderer, ActionListener, ItemListener {
+public abstract class ActivityEntry extends BaseTab implements IActivityEntryRenderer, ActionListener, ItemListener {
     protected static final String DATE_FORMAT = "yyyy/MM/dd";
 
     protected static final int X_SCALE = 20;
     protected static final int X_GAP = 0;
 
 //    protected JPanel panel;
-    private JTable activitiesTable;
+protected JTable activitiesTable;
     private JPanel charts;
     private JScrollPane tablePane;
     private JScrollPane chartPane;
@@ -109,57 +109,7 @@ public class ActivityEntry extends BaseTab implements IActivityEntryRenderer, Ac
 
     private void createUIComponents() {
 
-        tableRows = new Object[PMTable.MAX_TABLE_SIZE][];
-        columnNames = new String[]{PMTable.ID, PMTable.NAME, PMTable.DURATION, PMTable.START, PMTable.FINISH, PMTable.PREDECESSORS};
-        dtm = new PMTableModelGantt(tableRows, columnNames, getProjectManager());
-
-        dtm.clear();
-
-        DefaultTableColumnModel scm = new DefaultTableColumnModel();
-
-        activitiesTable = new PMTable(dtm, scm, this);
-        activitiesTable.setCellSelectionEnabled(true);
-        activitiesTable.addMouseListener(new PopClickListener());
-
-        activitiesTable.createDefaultColumnsFromModel();
-
-        activitiesTable.getColumn(PMTable.DURATION).setCellEditor(new IntegerEditor(1, PMTable.MAX_TABLE_SIZE, this));
-        activitiesTable.getColumn(PMTable.START).setCellEditor(new DatePickerCellEditor(new SimpleDateFormat(DATE_FORMAT)));
-        activitiesTable.getColumn(PMTable.FINISH).setCellEditor(new DatePickerCellEditor(new SimpleDateFormat(DATE_FORMAT)));
-        activitiesTable.getColumn(PMTable.PREDECESSORS).setCellEditor(new PredecessorEditor(this));
-
-        JTableHeader header = activitiesTable.getTableHeader();
-        header.setPreferredSize(new Dimension(10, 38));
-
-        activitiesTable.getColumn(PMTable.ID).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
-        activitiesTable.getColumn(PMTable.DURATION).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
-        activitiesTable.getColumn(PMTable.START).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
-        activitiesTable.getColumn(PMTable.FINISH).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
-        activitiesTable.getColumn(PMTable.PREDECESSORS).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
-
-        activitiesTable.getColumn(PMTable.ID).sizeWidthToFit();
-        activitiesTable.getColumn(PMTable.DURATION).sizeWidthToFit();
-        activitiesTable.getColumn(PMTable.START).sizeWidthToFit();
-        activitiesTable.getColumn(PMTable.FINISH).sizeWidthToFit();
-        activitiesTable.getColumn(PMTable.PREDECESSORS).sizeWidthToFit();
-
-        TableCellRenderer tableCellRenderer = new DefaultTableCellRenderer() {
-
-            SimpleDateFormat f = new SimpleDateFormat(DATE_FORMAT);
-
-            public Component getTableCellRendererComponent(JTable table,
-                                                           Object value, boolean isSelected, boolean hasFocus,
-                                                           int row, int column) {
-                if (value instanceof Date) {
-                    value = f.format(value);
-                }
-                return super.getTableCellRendererComponent(table, value, isSelected,
-                        hasFocus, row, column);
-            }
-        };
-
-        activitiesTable.getColumn(PMTable.START).setCellRenderer(tableCellRenderer);
-        activitiesTable.getColumn(PMTable.FINISH).setCellRenderer(tableCellRenderer);
+        createEntryColumns();
 
         activitiesTable.setGridColor(new Color(211, 211, 211));
         activitiesTable.setPreferredScrollableViewportSize(new Dimension(450, 400));
@@ -251,6 +201,8 @@ public class ActivityEntry extends BaseTab implements IActivityEntryRenderer, Ac
 
         };
     }
+
+    protected abstract void createEntryColumns();
 
     public void autoLayout(mxGraph graph) {
         charts.remove(graphComponent);
--- src/edu/concordia/comp354/gui/GanttTab.java
+++ src/edu/concordia/comp354/gui/GanttTab.java
@@ -1,7 +1,18 @@
 package edu.concordia.comp354.gui;
 
+import edu.concordia.comp354.gui.editors.IntegerEditor;
+import edu.concordia.comp354.gui.editors.PredecessorEditor;
+import org.jdesktop.swingx.table.DatePickerCellEditor;
+import sun.swing.table.DefaultTableCellHeaderRenderer;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class GanttTab extends ActivityEntry  {
 
@@ -41,5 +52,59 @@ public class GanttTab extends ActivityEntry  {
 
     @Override
     public void gainFocus() {
+    }
+
+    protected void createEntryColumns() {
+        tableRows = new Object[PMTable.MAX_TABLE_SIZE][];
+        columnNames = new String[]{PMTable.ID, PMTable.NAME, PMTable.DURATION, PMTable.START, PMTable.FINISH, PMTable.PREDECESSORS};
+        dtm = new PMTableModelGantt(tableRows, columnNames, getProjectManager());
+
+        dtm.clear();
+
+        DefaultTableColumnModel scm = new DefaultTableColumnModel();
+
+        activitiesTable = new PMTable(dtm, scm, this);
+        activitiesTable.setCellSelectionEnabled(true);
+        activitiesTable.addMouseListener(new PopClickListener());
+
+        activitiesTable.createDefaultColumnsFromModel();
+
+        activitiesTable.getColumn(PMTable.DURATION).setCellEditor(new IntegerEditor(1, PMTable.MAX_TABLE_SIZE, this));
+        activitiesTable.getColumn(PMTable.START).setCellEditor(new DatePickerCellEditor(new SimpleDateFormat(DATE_FORMAT)));
+        activitiesTable.getColumn(PMTable.FINISH).setCellEditor(new DatePickerCellEditor(new SimpleDateFormat(DATE_FORMAT)));
+        activitiesTable.getColumn(PMTable.PREDECESSORS).setCellEditor(new PredecessorEditor(this));
+
+        JTableHeader header = activitiesTable.getTableHeader();
+        header.setPreferredSize(new Dimension(10, 38));
+
+        activitiesTable.getColumn(PMTable.ID).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
+        activitiesTable.getColumn(PMTable.DURATION).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
+        activitiesTable.getColumn(PMTable.START).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
+        activitiesTable.getColumn(PMTable.FINISH).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
+        activitiesTable.getColumn(PMTable.PREDECESSORS).setHeaderRenderer(new DefaultTableCellHeaderRenderer());
+
+        activitiesTable.getColumn(PMTable.ID).sizeWidthToFit();
+        activitiesTable.getColumn(PMTable.DURATION).sizeWidthToFit();
+        activitiesTable.getColumn(PMTable.START).sizeWidthToFit();
+        activitiesTable.getColumn(PMTable.FINISH).sizeWidthToFit();
+        activitiesTable.getColumn(PMTable.PREDECESSORS).sizeWidthToFit();
+
+        TableCellRenderer tableCellRenderer = new DefaultTableCellRenderer() {
+
+            SimpleDateFormat f = new SimpleDateFormat(DATE_FORMAT);
+
+            public Component getTableCellRendererComponent(JTable table,
+                                                           Object value, boolean isSelected, boolean hasFocus,
+                                                           int row, int column) {
+                if (value instanceof Date) {
+                    value = f.format(value);
+                }
+                return super.getTableCellRendererComponent(table, value, isSelected,
+                        hasFocus, row, column);
+            }
+        };
+
+        activitiesTable.getColumn(PMTable.START).setCellRenderer(tableCellRenderer);
+        activitiesTable.getColumn(PMTable.FINISH).setCellRenderer(tableCellRenderer);
     }
 }
