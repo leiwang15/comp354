901ee54fc8a8548f54247fae4fcd6cd66b44040c
Joao Miranda <jmiranda@masterfile.com>
2015-08-01 16:18:59 -0400

    Added red  fill-in to represent probability of failure.

--- src/edu/concordia/comp354/gui/PERT/PERTBoxShape.java
+++ src/edu/concordia/comp354/gui/PERT/PERTBoxShape.java
@@ -49,9 +49,12 @@ public class PERTBoxShape extends mxRectangleShape {
                     int fillWidth = (int) (probability * rect.getWidth()/2);
                     Color oldColor = graphics.getColor();
                     graphics.setColor(new Color(210, 255, 199));
-                    int radius = getArcSize((int)rect.getWidth(), (int)rect.getHeight());
 
-                    graphics.fillRoundRect((int) rect.x + rect.width / 2 + 2, (int) rect.getY() + 1, fillWidth - 2, (int) rect.height / 2 - 1, radius,radius);
+                    int radius = getArcSize((int)rect.getWidth(), (int)rect.getHeight());
+                    graphics.fillRoundRect(rect.x + rect.width / 2 + 2, (int) rect.getY() + 1, fillWidth - 2, rect.height / 2 - 1, radius,radius);
+
+                    graphics.setColor(new Color(255, 222, 211));
+                    graphics.fillRoundRect(rect.x + rect.width / 2 + fillWidth, (int) rect.getY() + 1, (int)rect.getWidth() - fillWidth - 2, rect.height / 2 - 1, radius,radius);
 
                     graphics.setColor(oldColor);
 
